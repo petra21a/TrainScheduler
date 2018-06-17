f456b716fe45559be7e0daf0f84ee67b7a4deb1a
petra21a <35877270+petra21a@users.noreply.github.com>
2018-06-17 00:06:30 -0500

    cpy frm activity next arrival working

--- assets/app.js
+++ assets/app.js
@@ -27,18 +27,21 @@ var config = {
 
   database.ref().on("child_added", function(snapshot){
     event.preventDefault();
-    
-    // let  tFrequency = 3;
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        let firstTimeConverted = moment(snapshot.val().firstTrain, "HH:mm").subtract(1, "years");
     //     // Difference between the times
-        let diffTime = moment().diff(moment(snapshot.val().firstTrain), "HH/MM");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
+    let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      // Time apart (remainder)
+    let tRemainder = diffTime % snapshot.val().trainFrequency;
+    let nextArrival = snapshot.val().trainFrequency - tRemainder;
+    let minutesAway = moment().add(nextArrival, "minutes");
 
     $("tbody").append("<tr><br>"+
     "<td>"+snapshot.val().trainName+"</td><br>"+
     "<td>"+snapshot.val().destination+"</td><br>"+
-    "<td>"+snapshot.val().trainFrequency+"</td><br>")
-    // "<td>"+nextArrival+"</td><br>"+
-    // "<td>"+minutesAway+"<td><br>")
+    "<td>"+snapshot.val().trainFrequency+"</td><br>"+
+    "<td>"+nextArrival+"</td><br>"+
+    "<td>"+minutesAway+"<td><br>")
   });
 
 
